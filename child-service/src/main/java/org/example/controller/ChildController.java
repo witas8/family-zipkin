0f348928dcb04947b4f89f2754fd439cfdcb3f39
miwitkow <mikolaj.witkowski@capgemini.com>
2024-02-16 14:39:51 +0100

    Loki connection attempt nr 1

--- child-service/src/main/java/org/example/controller/ChildController.java
+++ child-service/src/main/java/org/example/controller/ChildController.java
@@ -2,12 +2,14 @@ package org.example.controller;
 
 import io.micrometer.observation.annotation.Observed;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.service.ChildService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/child")
 @RequiredArgsConstructor
@@ -24,4 +26,11 @@ public class ChildController {
     public ResponseEntity<String> ask() {
         return ResponseEntity.ok(service.askFather());
     }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        log.info("hello");
+        return ResponseEntity.ok("Hello, it's Child!");
+    }
+
 }
